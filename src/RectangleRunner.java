a472e327272a538697467684fa21007f4b5f5dd2
Morsalin <morsalinmahmood@gmail.com>
2023-09-28 22:20:14 -0400

    second commit for total plot area

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -4,16 +4,13 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
 
-        System.out.println("Plot 1 length: " + plot1.getLength());
-        System.out.println("Plot 1 width: " + plot1.getWidth());
-        System.out.println("Plot 1 area: " + (plot1.getLength() * plot1.getWidth()));
 
-        System.out.println("Plot 2 length: " + plot2.getLength());
-        System.out.println("Plot 2 width: " + plot2.getWidth());
-        System.out.println("Plot 2 area: " + (plot2.getLength() * plot2.getWidth()));
+        plot1.setWidth(75);
+        plot2.setWidth(75);
+        plot3.setWidth(75);
+        plot2.setLength(75);
 
-        System.out.println("Plot 3 length: " + plot3.getLength());
-        System.out.println("Plot 3 width: " + plot3.getWidth());
-        System.out.println("Plot 3 area: " + (plot3.getLength() * plot3.getWidth()));
+        int totalArea = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
+        System.out.println("These three plots requires " + totalArea + " square feet of seed");
     }
 }
